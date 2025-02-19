6c27823669659d0f34e31bccc616e4fc0fecae89
Physic69 <vencutsub11111111111@gmail.com>
2025-02-19 18:08:58 +0530

    Add files via upload

--- database.h
+++ database.h
@@ -1,132 +1,139 @@
-#ifndef DATABASE_H
-#define DATABASE_H
+#ifndef DATABASE1_H
+#define DATABASE1_H
 
 #include <iostream>
 #include <fstream>
-#include <filesystem>
-#include <unordered_map>
-#include <nlohmann/json.hpp>
+#include <cstring>
 
 using namespace std;
-namespace fs = filesystem;
 
-class Database {
-private:
-    string db_path;
-
-public:
-    Database(const string& db_name) {
-        db_path = "./" + db_name;
-        if (!fs::exists(db_path)) {
-            fs::create_directory(db_path);
-        }
-    }
-
-    void createTable(const string& table_name) {
-        string table_file = db_path + "/" + table_name + ".json";
-        if (!fs::exists(table_file)) {
-            ofstream file(table_file);
-            nlohmann::json empty_json = {};
-            file << empty_json.dump(4);
-            file.close();
-            cout << "Table '" << table_name << "' created successfully.\n";
-        } else {
-            cout << "Table '" << table_name << "' already exists.\n";
-        }
-    }
-
-    void insertData(const string& table_name, const string& key, const string& value) {
-        string table_file = db_path + "/" + table_name + ".json";
-        ifstream file_in(table_file);
-        if (!file_in) {
-            cout << "Table does not exist.\n";
-            return;
-        }
-
-        nlohmann::json table_data;
-        file_in >> table_data;
-        file_in.close();
-
-        if (table_data.contains(key)) {
-            cout << "Key already exists!\n";
-            return;
-        }
-
-        table_data[key] = value;
-        ofstream file_out(table_file);
-        file_out << table_data.dump(4);
-        file_out.close();
-        cout << "Data inserted successfully.\n";
-    }
-
-    void listTables() {
-        for (const auto& entry : fs::directory_iterator(db_path)) {
-            cout << "Table: " << entry.path().filename().stem().string() << "\n";
-        }
-    }
-
-    void displayTable(const string& table_name) {
-        string table_file = db_path + "/" + table_name + ".json";
-        ifstream file(table_file);
-        if (!file) {
-            cout << "Table does not exist.\n";
-            return;
-        }
-
-        nlohmann::json table_data;
-        file >> table_data;
-        file.close();
-
-        cout << table_data.dump(4) << "\n";
-    }
-
-    void updateEntry(const string& table_name, const string& key, const string& new_value) {
-        string table_file = db_path + "/" + table_name + ".json";
-        ifstream file_in(table_file);
-        if (!file_in) {
-            cout << "Table does not exist.\n";
-            return;
-        }
-
-        nlohmann::json table_data;
-        file_in >> table_data;
-        file_in.close();
-
-        if (!table_data.contains(key)) {
-            cout << "Key does not exist!\n";
-            return;
-        }
-
-        table_data[key] = new_value;
-        ofstream file_out(table_file);
-        file_out << table_data.dump(4);
-        file_out.close();
-        cout << "Data updated successfully.\n";
-    }
-
-    void deleteEntry(const string& table_name, const string& key) {
-        string table_file = db_path + "/" + table_name + ".json";
-        ifstream file_in(table_file);
-        if (!file_in) {
-            cout << "Table does not exist.\n";
-            return;
-        }
-
-        nlohmann::json table_data;
-        file_in >> table_data;
-        file_in.close();
-
-        if (!table_data.contains(key)) {
-            cout << "Key does not exist!\n";
-            return;
-        }
-
-        table_data.erase(key);
-        ofstream file_out(table_file);
-        file_out << table_data.dump(4);
-        file_out.close();
-        cout << "Data deleted successfully.\n";
-    }
+struct Record {
+    int key;
+    char value[100];
 };
 
-#endif // DATABASE_H
+bool keyExists(const string& tableName, int searchKey) {
+    ifstream file(tableName, ios::binary);
+    if (!file) return false;
+
+    Record record;
+    while (file.read(reinterpret_cast<char*>(&record), sizeof(Record))) {
+        if (record.key == searchKey) {
+            file.close();
+            return true;
+        }
+    }
+    file.close();
+    return false;
+}
+
+void insertRecord(const string& tableName, int key, const string& value) {
+    if (keyExists(tableName, key)) {
+        cout << "Error: Key " << key << " already exists!\n";
+        return;
+    }
+    
+    ofstream file(tableName, ios::binary | ios::app);
+    if (!file) {
+        cerr << "Error opening file for writing!\n";
+        return;
+    }
+
+    Record record = {key, ""};
+    strncpy(record.value, value.c_str(), sizeof(record.value) - 1);
+    file.write(reinterpret_cast<char*>(&record), sizeof(Record));
+    file.close();
+}
+
+void listRecords(const string& tableName) {
+    ifstream file(tableName, ios::binary);
+    if (!file) {
+        cerr << "Error opening file for reading!\n";
+        return;
+    }
+
+    Record record;
+    cout << "---- Table Data (" << tableName << ") ----\n";
+    while (file.read(reinterpret_cast<char*>(&record), sizeof(Record))) {
+        cout << "Key: " << record.key << " | Value: " << record.value << endl;
+    }
+    file.close();
+}
+
+void searchRecord(const string& tableName, int searchKey) {
+    ifstream file(tableName, ios::binary);
+    if (!file) {
+        cerr << "Error opening file for reading!\n";
+        return;
+    }
+    
+    Record record;
+    while (file.read(reinterpret_cast<char*>(&record), sizeof(Record))) {
+        if (record.key == searchKey) {
+            cout << "Found! Key: " << record.key << " | Value: " << record.value << endl;
+            return;
+        }
+    }
+    cout << "Record not found!\n";
+    file.close();
+}
+
+void updateRecord(const string& tableName, int searchKey, const string& newValue) {
+    fstream file(tableName, ios::binary | ios::in | ios::out);
+    if (!file) {
+        cerr << "Error opening file for updating!\n";
+        return;
+    }
+    
+    Record record;
+    while (file.read(reinterpret_cast<char*>(&record), sizeof(Record))) {
+        if (record.key == searchKey) {
+            strncpy(record.value, newValue.c_str(), sizeof(record.value) - 1);
+            file.seekp(-static_cast<int>(sizeof(Record)), ios::cur);
+            file.write(reinterpret_cast<char*>(&record), sizeof(Record));
+            file.close();
+            cout << "Record updated!\n";
+            listRecords(tableName);
+            return;
+        }
+    }
+    
+    cout << "Record not found!\n";
+    file.close();
+}
+
+void deleteRecord(const string& tableName, int deleteKey) {
+    ifstream file(tableName, ios::binary);
+    if (!file) {
+        cerr << "Error opening file for deleting!\n";
+        return;
+    }
+
+    ofstream temp("temp.dat", ios::binary);
+    Record record;
+    bool found = false;
+    
+    while (file.read(reinterpret_cast<char*>(&record), sizeof(Record))) {
+        if (record.key == deleteKey) {
+            found = true;
+        } else {
+            temp.write(reinterpret_cast<char*>(&record), sizeof(Record));
+        }
+    }
+    
+    file.close();
+    temp.close();
+    
+    remove(tableName.c_str());
+    rename("temp.dat", tableName.c_str());
+    
+    if (found) {
+        cout << "Record deleted!\n";
+        listRecords(tableName);
+    } else {
+        cout << "Record not found!\n";
+    }
+}
+
+#endif // DATABASE1_H
+
